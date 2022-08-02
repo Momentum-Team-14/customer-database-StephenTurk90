8a8569824be679b34d473ea14f0329b3551c1ade
StephenTurk90 <stephenjturk@gmail.com>
2022-08-01 21:03:57 -0400

    made name div

--- momentum.js
+++ momentum.js
@@ -1,1 +1,16 @@
 console.log("hi!")
+
+const container = document.querySelector(".container")
+console.log(customers)
+function showName(customerArray) {
+    for (let customer of customerArray){
+        let customerDiv = document.createElement('div')
+        customerDiv.classList.add('customer')
+        let nameDiv = document.createElement('div')
+        nameDiv.classList.add('name')
+        nameDiv.innerText = `${customer.name.first} ${customer.name.last}`
+        customerDiv.appendChild(nameDiv)
+        container.appendChild(customerDiv)
+    }
+}
+// name: first and last
